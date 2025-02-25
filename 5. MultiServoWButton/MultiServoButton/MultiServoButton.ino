00913b435c6ad799f12ac2adfe4ad23321cdc4be
Kallum <kallum179@gmail.com>
2025-02-25 19:46:09 +0000

    servos now move per tick rather than all in one tick

--- 5. MultiServoWButton/MultiServoButton/MultiServoButton.ino
+++ 5. MultiServoWButton/MultiServoButton/MultiServoButton.ino
@@ -7,46 +7,81 @@ const int buttonPin = 2;  //D2
 bool buttonPressed = false;
 
 struct PopUp {
-
   static const int minAngle = 150;
   static const int maxAngle = 300;
-  static const int delayMs = 50;
+  static const int dAngle = 5;
+  static const int delayMs = 5;
 
-  int pin;
+  int registeredPin;
   
-  int servoPosition = 0;
-
+  int moving;
+  int poppedUp;
+  int currentAngle;
 
   PopUp(int _pin) {
-    pin = _pin;
-    pwm.setPWM(pin, 0, minAngle);
+    registeredPin = _pin;
+
+    moving = 0;
   }
 
-  void Toggle() {
-    if (servoPosition)
-      MoveServoDown();
-    else
-      MoveServoUp();
+  void ResetPositions() {
+    pwm.setPWM(registeredPin, 0, minAngle);
+    currentAngle = minAngle;
+    poppedUp = 0;
   }
+
+  void RegisterButtonPress() {
+    moving = 1;
+  }
+
+  void Tick() {
+    if (moving) {
+      if (poppedUp)
+        MoveServoDown();
+      else
+        MoveServoUp();
+    }
+  }
+
   void MoveServoUp() {
-    for (int i = PopUp::minAngle; i <= PopUp::maxAngle; i+= 50) {
-      pwm.setPWM(pin, 0, i);
+    if (currentAngle < PopUp::maxAngle) { //if there was more movement left in this period
+      currentAngle += dAngle; //set new angle to move to
+      if (currentAngle > PopUp::maxAngle) { //don't allow more than the maximum
+        currentAngle = PopUp::maxAngle;
+        Serial.print("Had to constrain to maximum for: ");
+        Serial.println(registeredPin);
+      }
+
+      pwm.setPWM(registeredPin, 0, currentAngle);
       delay(PopUp::delayMs);
+
+    } else {
+      moving = false;
+      poppedUp = 1;
     }
-    servoPosition = 1;
   }
+
   void MoveServoDown() {
-    for (int i = PopUp::maxAngle; i >= PopUp::minAngle; i-=50) {
-      pwm.setPWM(pin, 0, i);
+    if (currentAngle > PopUp::minAngle) { //if there was more movement left in this period
+      currentAngle -= dAngle; //set new angle to move to
+      if (currentAngle < PopUp::minAngle) { //don't allow less than the minimum
+        currentAngle = PopUp::minAngle;
+        Serial.print("Had to constrain to minimum for: ");
+        Serial.println(registeredPin);
+      }
+
+      pwm.setPWM(registeredPin, 0, currentAngle); 
       delay(PopUp::delayMs);
+
+    } else {
+      moving = false;
+      poppedUp = 0;
     }
-    servoPosition = 0;
   }
 };
 
 PopUp p1 = PopUp(0);
 PopUp p2 = PopUp(1);
-
 
 void setup() {
   Serial.begin(9600);
@@ -56,6 +91,9 @@ void setup() {
   pwm.setPWMFreq(50); // Set frequency to 50Hz for servos
 
   pinMode(buttonPin, INPUT_PULLUP); // Enable internal pull-up resistor
+
+  p1.ResetPositions();
+  p2.ResetPositions();
 }
 
 void loop() {
@@ -64,11 +102,14 @@ void loop() {
     if (!buttonPressed) { 
       buttonPressed = true;
       
-      p1.Toggle();
-      p2.Toggle();
+      p1.RegisterButtonPress();
+      p2.RegisterButtonPress();
     }
   } else {
       buttonPressed = false; // Reset trigger when button is released
       delay(10);
   }
+
+  p1.Tick();
+  p2.Tick();
 }
